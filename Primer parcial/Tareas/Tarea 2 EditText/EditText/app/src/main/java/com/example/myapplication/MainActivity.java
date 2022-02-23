b37485b30202e949e3863611d714faa6b5073820
Carlos Santiago <nwasantiago611@gmail.com>
2022-02-22 18:52:24 -0600

    Aun no termino la tarea 2

--- Primer parcial/Tareas/Tarea 2 EditText/EditText/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Primer parcial/Tareas/Tarea 2 EditText/EditText/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.*;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle b) {
         super.onCreate(b);
         setContentView(R.layout.activity_main);
-        EditText campo1 = (EditText) findViewById(R.id.xet1);
-        Log.d("Obteniendo cursor...", String.valueOf(campo1.getSelectionEnd()));
+        EditText jet1 = (EditText) findViewById(R.id.xet1);
+        jet1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    ImageView iconoDescuento = (ImageView) findViewById(R.id.xiv1);
+                    Drawable d = iconoDescuento.getDrawable();
+                    d = DrawableCompat.wrap(d);
+                    DrawableCompat.setTint(d,
+                            ContextCompat.getColor(MainActivity.this, R.color.micolor));
+                } // micolor es un archivo xml en la carpeta color.xml.
+            }
+        });
     }
 }
+
