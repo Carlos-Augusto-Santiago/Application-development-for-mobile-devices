32e52066e1bace3ec052a57a4c76c0c451a4f356
Carlos Santiago <nwasantiago611@gmail.com>
2022-02-24 19:36:37 -0600

    Tarea 6 Se cambia la vista para los planetas y falta acomodar el boton

--- Primer parcial/Tareas/Tarea 6/Listas/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Primer parcial/Tareas/Tarea 6/Listas/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,22 +12,24 @@ public class MainActivity extends Activity {
         super.onCreate(b);
         setContentView(R.layout.listado);
         ArrayList<ListaEntrada> al = new ArrayList<ListaEntrada>();
-        al.add(new ListaEntrada(R.drawable.buho,
-                "Mercurio", "mer"));
-        al.add(new ListaEntrada(R.drawable.colibri,
-                "Venus", "ven"));
-        al.add(new ListaEntrada(R.drawable.cuervo,
-                "Tierra", "tierra"));
-        al.add(new ListaEntrada(R.drawable.flamenco,
-                "Marte", "mars"));
-        al.add(new ListaEntrada(R.drawable.kiwi,
-                "Jupiter", "jup"));
-        al.add(new ListaEntrada(R.drawable.loro,
-                "Saturno", "satur"));
-        al.add(new ListaEntrada(R.drawable.pavo,
-                "Urano", "uranius"));
-        al.add(new ListaEntrada(R.drawable.pinguino,
-                "Neptuno", "npth"));
+        al.add(new ListaEntrada(R.drawable.mercurio,
+                "Mercurio", "Mercurio es el planeta del sistema solar más cercano al Sol y el más pequeño. Forma parte de los denominados planetas interiores y carece de satélites naturales al igual que Venus."));
+        al.add(new ListaEntrada(R.drawable.venus,
+                "Venus", "Venus es el segundo planeta del sistema solar en orden de proximidad al Sol y el tercero en cuanto a tamaño en orden ascendente después de Mercurio y Marte. Al igual que Mercurio, carece de satélites naturales. Recibe su nombre en honor a Venus, la diosa romana del amor."));
+        al.add(new ListaEntrada(R.drawable.tierra,
+                "Tierra", "Nuestro hogar, el planeta Tierra, es un planeta terrestre y rocoso. Tiene una superficie sólida y activa, con montañas, valles, cañones, llanuras y mucho más. La Tierra es especial porque es un planeta océano, ya que el agua cubre el 70% de su superficie. Nuestra atmósferaestá compuesta, en gran parte, por nitrógeno."));
+        al.add(new ListaEntrada(R.drawable.marte,
+                "Marte", "Marte es el cuarto planeta en orden de distancia al Sol y el segundo más pequeño del sistema solar, después de Mercurio"));
+        al.add(new ListaEntrada(R.drawable.jupiter,
+                "Jupiter", "Júpiter es el planeta más grande del sistema solar y el quinto en orden de lejanía al Sol. Es un gigante gaseoso que forma parte de los denominados planetas exteriores. Recibe su nombre del dios romano Júpiter"));
+        al.add(new ListaEntrada(R.drawable.saturno,
+                "Saturno", "Saturno es el sexto planeta del sistema solar contando desde el Sol, el segundo en tamaño y masa después de Júpiter y el único con un sistema de anillos visible desde la Tierra. Su nombre proviene del dios romano Saturno. Forma parte de los denominados planetas exteriores o gaseosos."));
+        al.add(new ListaEntrada(R.drawable.urano,
+                "Urano", "Urano es el séptimo planeta del sistema solar, el tercero de mayor tamaño, y el cuarto más masivo. Se llama así en honor de la divinidad griega del cielo Urano, el padre de Crono y el abuelo de Zeus."));
+        al.add(new ListaEntrada(R.drawable.neptuno,
+                "Neptuno", "Neptuno es el octavo planeta en distancia respecto al Sol y el más lejano del sistema solar. Forma parte de los denominados planetas exteriores, y dentro de estos, es uno de los gigantes helados, y es el primero que fue descubierto gracias a predicciones matemáticas."));
+        al.add(new ListaEntrada(R.drawable.pluton,
+                "Pluton", "Plutón, designado Pluto, es un planeta enano del sistema solar situado a continuación de la órbita de Neptuno. Su nombre se debe al dios mitológico romano Plutón."));
         lv = (ListView) findViewById(R.id.xlv_listado);
         lv.setAdapter(new ListaAdapter(this, R.layout.activity_main, al) {
             public void onEntrada(Object o, View v) {
@@ -43,7 +45,9 @@ public class MainActivity extends Activity {
                         imagen_entrada.setImageResource(((ListaEntrada) o).get_idImagen());
                 }
             }
-        });
+        }
+            
+        );
         lv.setOnItemClickListener(new OnItemClickListener() {
             public void onItemClick(AdapterView<?> av, View view, int i, long l) {
                 ListaEntrada le = (ListaEntrada) av.getItemAtPosition(i);
