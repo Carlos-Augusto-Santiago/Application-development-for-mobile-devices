dd4d0ae6f8fe8f87059da1bfec841c9a989b4fea
Carlos Santiago <carlos@thedisruptoragency.com>
2022-04-20 13:09:57 -0500

    tarea 22

--- Tareas/Tarea 22/Mapas/app/src/main/java/com/example/myapplication/MapsActivity.java
+++ Tareas/Tarea 22/Mapas/app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -2,8 +2,16 @@ package com.example.myapplication;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.location.Geocoder;
 import android.location.GpsSatellite;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -14,6 +22,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
+    private Button btnS, btnC, btnA, btnM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +31,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
+        btnS = (Button) findViewById(R.id.xbtnS);
+        btnC = (Button) findViewById(R.id.xbtnC);
+        btnA = (Button) findViewById(R.id.xbtnA);
+        btnM = (Button) findViewById(R.id.xbtnM);
     }
 
     /**
@@ -40,8 +52,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng mexico = new LatLng(19.42847, -99.12766);
         mMap.addMarker(new MarkerOptions().position(mexico).title("Marker in Mexico"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(mexico));
-        mMap.setMapType(mMap.MAP_TYPE_SATELLITE);
-        mMap.isTrafficEnabled();
 
+        btnS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mMap.getMapType() == GoogleMap.MAP_TYPE_SATELLITE){
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                }
+                else{
+                    mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                }
+            }
+        });
+
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LatLng sevilla = new LatLng(37.3826, -5.99629);
+                mMap.addMarker(new MarkerOptions().position(sevilla).title("Marker in Sevilla"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sevilla));
+                mMap.animateCamera( CameraUpdateFactory.zoomTo( 10.0f ) );
+            }
+        });
+
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LatLng sevilla = new LatLng(37.3826, -5.99629);
+                mMap.addMarker(new MarkerOptions().position(sevilla).title("Marker in Sevilla"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sevilla));
+                float zoom = mMap.getCameraPosition().zoom;
+                for(float i=zoom; i<15.0; i++) mMap.animateCamera( CameraUpdateFactory.zoomTo( i ) );
+            }
+        });
+
+        btnM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMap.moveCamera(CameraUpdateFactory.scrollBy(40,40));
+            }
+        });
     }
 }
---
+++ Tareas/Tarea 22/Mapas/app/src/main/java/com/example/myapplication/OverlayMapa.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.Projection;
+
+public class OverlayMapa extends Overlay {
+    private Double latitud = 37.40 * 1E6;
+    private Double longitud = -5.99 * 1E6;
+
+    public void draw(Canvas canvas, MapView mapView, boolean shadow) {
+        Projection projection = mapView.getProjection();
+        GeoPoint geoPoint = new GeoPoint(latitud.intValue(), longitud.intValue());
+        if (shadow == false) {
+            Point centro = new Point();
+            projection.toPixels(geoPoint, centro);
+            Paint p = new Paint();
+            p.setColor(Color.BLUE);
+            canvas.drawCircle(centro.x, centro.y, 5, p);
+            canvas.drawText("Sevilla", centro.x + 10, centro.y + 5, p);
+        }
+    }
+}
