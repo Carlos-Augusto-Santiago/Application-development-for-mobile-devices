84ccb9e374880e0b0d9fd7d913bdd7cdc0a93538
Carlos Santiago <carlos@thedisruptoragency.com>
2022-05-31 20:37:50 -0500

    ok

--- Tareas/TareaEnviarMensaje/app2/app/src/main/java/com/example/sms/ListContacts.java
+++ Tareas/TareaEnviarMensaje/app2/app/src/main/java/com/example/sms/ListContacts.java
@@ -1,6 +1,27 @@
 package com.example.sms;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.R;
 
-public class ListContacts extends AppCompatActivity {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+
+/** A simple two line list item. */
+public class ListContacts extends SingleLineItemViewHolder {
+
+    public final TextView secondary;
+
+    public TwoLineItemViewHolder(@NonNull View view) {
+        super(view);
+        this.secondary = itemView.findViewById(R.id.mtrl_list_item_secondary_text);
+    }
+
+    @NonNull
+    public static TwoLineItemViewHolder create(@NonNull ViewGroup parent) {
+        return new TwoLineItemViewHolder(
+                LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout., parent, false));
+    }
 }
